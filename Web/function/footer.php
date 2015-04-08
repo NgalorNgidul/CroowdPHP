d9897e2c417b4ce272a8cea3c7436655d0e39a4e
Syahrial Fandrianah <sfandrianah2@gmail.com>
2015-04-08 22:37:22 +0700

    SFANDRIANAH || FOOTER UNTUK CROOWD

--- Web/function/footer.php
+++ Web/function/footer.php
@@ -1,13 +1,13 @@
 <div class="container_12 main-footer">
-    <div class="grid_3 about-us">
+    <div class="grid_4 about-us">
         <h3 class="rs title">Alamat kami</h3>
         <p class="rs description">Alamat surat menyurat bisa ditujukan ke PT. DIGI LARAS PROSPERINDO, Epicentrum Walk Office Suite 
-	7th Floor Unit 0709A, Jl. H R Rasuna Said, Jakarta 12960</p>
+            7th Floor Unit 0709A, Jl. H R Rasuna Said, Jakarta 12960</p>
         <p class="rs email"><a class="fc-default  be-fc-orange" href="mailto:info@croowd.co.id">info@croowd.co.id</a></p>
         <p class="rs">+62 (21) 555 - 55 - 55</p>
     </div><!--end: .contact-info -->
     <div class="clear clear-2col"></div>
-    <div class="grid_3 email-newsletter">
+    <div class="grid_4 email-newsletter">
         <h3 class="rs title">Berlangganan Newsletter</h3>
         <div class="inner">
             <p class="rs description">Bagi anda yang ingin mendapatkan informasi teraktual tentang croowdfunding syariah, daftarkan email anda di bawah ini.</p>
@@ -21,7 +21,7 @@
             </form>
         </div>
     </div><!--end: .email-newsletter -->
-    <div class="grid_3">
+    <div class="grid_4">
         <h3 class="rs title">Informasi</h3>
         <div class="footer-menu">
             <ul class="rs">
@@ -35,6 +35,7 @@
     </div>
     <div class="clear"></div>
 </div>
+<br/>
 <div class="copyright">
     <div class="container_12">
         <div class="grid_12">
