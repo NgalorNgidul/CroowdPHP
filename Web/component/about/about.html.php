54d1be5342174c5121a3dffe0b46b8ed1e9eefd1
Isht <ae.isht@gmail.com>
2015-04-08 09:08:36 +0700

    Add dummy menu content for tentang kami and cara kerja

---
+++ Web/component/about/about.html.php
@@ -0,0 +1,35 @@
+<div class="layout-2cols">
+    <div class="content grid_8">
+        <div class="single-page">
+            <div class="wrapper-box box-post-comment">
+                <h2 class="common-title">Tentang Kami</h2>
+                <div class="box-white">
+                    <p class="rs pb30">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae tristique eros, nec auctor erat. Curabitur arcu magna, auctor eget nulla id, facilisis maximus lorem. Aenean sed dui sed ex tristique ullamcorper. Nam purus leo, aliquam id dui id, gravida iaculis urna. Mauris finibus turpis in bibendum dignissim. Pellentesque vitae arcu vitae purus sodales consequat ut eu ante. Aenean neque sem, convallis non gravida sit amet, efficitur et lacus. Aliquam dapibus lectus ac accumsan posuere. Nam semper fermentum nisi, vel pretium libero consequat ac. Proin dapibus elit quis consequat vestibulum. Curabitur sem mauris, sollicitudin eu imperdiet at, tincidunt quis quam. In eget arcu id turpis facilisis porta a at odio.</p>
+                    <p class="rs pb30">Etiam ac ligula congue, ultricies orci ut, porta ex. Vestibulum eu blandit elit. Pellentesque quis consectetur orci. Ut nec justo nec lorem lobortis suscipit. Ut cursus sagittis libero id bibendum. Fusce ullamcorper dictum lectus eget aliquet. Praesent sodales felis ac porta blandit.</p>
+                    <p class="rs pb30">Morbi condimentum bibendum tristique. Curabitur nunc dui, luctus molestie risus vitae, iaculis porttitor lorem. Etiam dapibus aliquet mauris et placerat. Sed augue sem, dictum id rutrum ut, tincidunt sed dolor. Aenean a velit sem. Mauris nec odio auctor, vestibulum purus et, fringilla velit. Aliquam sed neque mauris. Proin interdum a turpis in porta. Vivamus pellentesque turpis elit, a accumsan urna consequat vitae.</p>
+                    <p class="rs pb30">Donec lobortis neque id vulputate pulvinar. Donec tristique molestie blandit. Cras vitae ipsum iaculis, tempor nunc vitae, volutpat erat. Nullam elementum lorem ac euismod volutpat. Nam lacinia sem nibh, porttitor molestie orci scelerisque vitae. Vivamus fermentum felis sed purus fermentum, ut tincidunt erat vehicula. Duis molestie feugiat justo, eget pulvinar mi vestibulum eu. Nulla facilisi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas massa turpis, varius vitae tortor quis, tristique pretium turpis. Etiam condimentum velit ac mauris laoreet consectetur. Donec at lacinia ipsum. Proin eu consectetur diam, tincidunt placerat risus. Pellentesque vitae hendrerit odio.</p>
+                    <p class="rs pb30">Proin odio eros, accumsan eu tristique nec, lacinia vel dui. Mauris blandit, odio rhoncus pharetra convallis, tellus ante lacinia purus, vel pharetra arcu nibh quis sem. Mauris ullamcorper urna at tellus tempor facilisis. In dictum tellus sed erat luctus fringilla. Morbi congue at eros non tempus. Aliquam volutpat posuere massa vel aliquam. Morbi quis pretium magna. Donec vulputate, dolor eget dignissim congue, tellus ex lacinia neque, nec faucibus quam ante eu turpis. Nulla nec accumsan velit.</p>
+                </div>
+            </div>
+        </div><!--end: .box-list-comment -->
+    </div>
+</div><!--end: .content -->
+<div class="sidebar grid_4">
+    <div class="box-gray">
+        <h3 class="title-box">Contact info</h3>
+        <p class="rs description pb20">Pellentesque laoreet sapien id lacus luctus non fringilla elit lobortis. Fusce augue diam, tempor posuere pharetra sed, feugiat non sapien.</p>
+        <p class="rs pb20">
+            <span class="fw-b">Address</span>: 111 lorem St. 5th Floor,
+            Ipsum City, MA 00001
+        </p>
+        <p class="rs pb20">
+            <span class="fw-b">Phone</span>: +1 (555) 55-55-555
+            (9AM - 6PM EST)
+        </p>
+        <p class="rs pb20">
+            <span class="fw-b">Email</span>: <a href="mailto:info@megadrupal.com" class="be-fc-orange">info@megadrupal.com</a>
+        </p>
+    </div>
+</div><!--end: .sidebar -->
+<div class="clear"></div>
+</div>
