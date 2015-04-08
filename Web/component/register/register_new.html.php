921aceca8f561dda24d065fcfb5abb81928e2743
Syahrial Fandrianah <sfandrianah2@gmail.com>
2015-04-08 14:17:32 +0700

    SFANDRIANAH || UPDATE REGISTER

--- Web/component/register/register_new.html.php
+++ Web/component/register/register_new.html.php
@@ -7,24 +7,29 @@
             <label for="txt_name">
                 <input id="txt_name" class="txt fill-width" type="text" placeholder="Enter full name"/>
             </label>
-            <div class="wrap-2col clearfix">
-                <div class="col">
-                    <label for="txt_email">
-                        <input id="txt_email" class="txt fill-width" type="email" placeholder="Enter your e-mail address"/>
-                    </label>
-                    <label for="txt_re_email">
-                        <input id="txt_re_email" class="txt fill-width" type="email" placeholder="Re-enter your e-mail adress"/>
-                    </label>
-                </div>
-                <div class="col">
-                    <label for="txt_password">
-                        <input id="txt_password" class="txt fill-width" type="password" placeholder="Enter password"/>
-                    </label>
-                    <label for="txt_re_password">
-                        <input id="txt_re_password" class="txt fill-width" type="password" placeholder="Re-enter password"/>
-                    </label>
-                </div>
-            </div>
+            <label for="txt_email">
+                <input id="txt_email" class="txt fill-width" type="email" placeholder="Enter your e-mail address"/>
+            </label>
+            <label for="txt_re_email">
+                <input id="txt_re_email" class="txt fill-width" type="email" placeholder="Re-enter your e-mail adress"/>
+            </label>
+
+            <label for="txt_password">
+                <input id="txt_password" class="txt fill-width" type="password" placeholder="Enter password"/>
+            </label>
+            <label for="txt_re_password">
+                <input id="txt_re_password" class="txt fill-width" type="password" placeholder="Re-enter password"/>
+            </label>
+            <label for="txt_accept">
+                <input type="checkbox" value="1" required="required" name="member" id="member_accept_principles">
+            I agree to the Croowd Principles and Privacy Policy
+            </label>
+            <label for="txt_receive">
+                <input type="checkbox" value="1" checked="checked" required="required" name="receive" id="member_receive_principles">
+            I would like to receive occasional updates from Croowd
+            </label>
+            
+<br/>
             <p class="rs pb10">By signing up, you agree to our <a href="#" class="fc-orange">terms of use</a> and <a href="#" class="fc-orange">privacy policy</a>.</p>
             <p class="rs ta-c">
                 <button class="btn btn-red btn-submit" type="submit">Register</button>
