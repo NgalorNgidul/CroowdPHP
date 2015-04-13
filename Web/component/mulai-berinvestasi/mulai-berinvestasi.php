f367ebab3d341a7967e8c31ffb91e61e1e8a92ca
Syahrial Fandrianah <sfandrianah2@gmail.com>
2015-04-13 16:21:57 +0700

    SFANDRIANAH || SEND WITH METODE POST TO REST

--- Web/component/mulai-berinvestasi/mulai-berinvestasi.php
+++ Web/component/mulai-berinvestasi/mulai-berinvestasi.php
@@ -1,24 +1,27 @@
 
 <!--<script language="javascript"  type="text/javascript" src="component/cari-pinjaman/cari-pinjaman.js"></script>-->
 <?php
-
-if($_GET['kirim']=='kirim'){
+if ($_GET['kirim'] == 'kirim') {
     $url = "registration/create";
 //    $content = 'name=' . $_POST['name'] . '&email=' . $_POST['email']. '&invest=1';
-        $content = '{"name":"'. $_POST['name'] .'","email":"' . $_POST['email']. '","invest":1}';
+    $content = '{"name":"' . $_POST['name'] . '","email":"' . $_POST['email'] . '","invest":1}';
 
 //    {“name”:”nama_lengkap”,”email”:”alamat_email”}
-     //   echo $_POST['name'].','.$_POST['email'];
-    $response = sendPOSTDATA($url,$content);
-    echo $response;
-    echo 'Terima Kasih anda sudah melakukan pendaftaran';
-   // header( "refresh:5;location:?content=".$_GET['content']."" );
-    echo "<script>setTimeout(\"location.href = '?content=".$_GET['content']."';\",5000);</script>";
+    //   echo $_POST['name'].','.$_POST['email'];
+    $response = sendPOSTDATA($url, $content);
+
+    //   echo $response;
+    if ($response == '') {
+        echo 'Terima Kasih anda sudah melakukan pendaftaran';
+        echo "<script>setTimeout(\"location.href = '?content=" . $_GET['content'] . "';\",5000);</script>";
+    } else {
+        echo 'Gagal Melakukan Pendaftaran';
+        echo "<script>setTimeout(\"location.href = '?content=" . $_GET['content'] . "';\",5000);</script>";
+    }
 } else {
 
-include 'component/'.$_GET['content'].'/'.$_GET['content'].'.html.php';
+    include 'component/' . $_GET['content'] . '/' . $_GET['content'] . '.html.php';
 }
-
 ?>
 
- 
+
