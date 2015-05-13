629956da3938225e2557f82d6b33f0eff91e8412
Muhammad Ishthifa <ae.isht@gmail.com>
2015-05-13 10:08:39 +0700

    Commit with nano

---
+++ Web/function/latest-news.php
@@ -0,0 +1,13 @@
+<?php
+$data = bacaHTML('prospect/popular');
+$rows = json_decode($data);
+?>
+
+<div class="container_8 clearfix">
+    <div class="grid_8 wrap-title">
+        <h2 class="common-title">Berita <span class="fc-orange">Terbaru</span></h2>
+    </div>
+    Latest News Content
+    <div class="clear"></div>
+    <div class="clear clear-2col"></div>
+</div><!--end: .home-discover-friends -->
