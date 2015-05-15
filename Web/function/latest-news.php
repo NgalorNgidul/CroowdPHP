7019b4874c12fe4ab0306252f10e21660bb9aed0
Muhammad Ishthifa <ae.isht@gmail.com>
2015-05-15 09:32:15 +0700

    Change Berita Terbaru

--- Web/function/latest-news.php
+++ Web/function/latest-news.php
@@ -1,17 +1,16 @@
 <div class="container_12 clearfix">
     <div class="project-short">
-        <div class="top-project-info">
+        <div class="top-project-info" style="padding: 10px 10px 15px 10px !important">
             <div class="content-info-short clearfix">
-                <div class="grid_4">
-                    <a class="thumb-img" href="#" style="height:204px !important;width: 292px !important">
+                <div class="grid_3 clearfix" style="margin-right: 100px !important">
+                    <a class="thumb-img" href="#" style="height:204px !important;width: 292px !important; padding-bottom: 3px;">
                         <img alt="$TITLE" src="images/projects/small/2.jpg">
                     </a>
                 </div>
-                <div class="clear"></div>
-                <div class="grid_8">
-                    <div class="wrap-short-detail">
-                        <h3 class="rs acticle-title"><a href="#" class="be-fc-orange">News Title</a></h3>
-                        <p class="rs title-description">News Descriptions</p>
+                <div class="grid_7">
+                    <div class="wrap-short-detail" style="width: 100%">
+                        <h3 class="rs acticle-title"><a href="#" class="be-fc-orange">Lorem Ipsum</a></h3>
+                        <p class="rs title-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi convallis erat eu fermentum malesuada. Nulla facilisi. Etiam venenatis turpis diam, ut gravida leo porta malesuada. Suspendisse nulla odio, commodo id feugiat et, tincidunt at sem. Nullam urna eros, malesuada a eros nec, tempus consectetur ligula. Ut maximus tortor ac diam venenatis aliquam. Maecenas maximus tellus finibus scelerisque placerat. Pellentesque sit amet velit vehicula, consectetur erat non, condimentum nisl. Nullam maximus hendrerit quam, et vulputate dui molestie ut. Etiam sodales eu risus vitae efficitur. <a href="#" class="be-fc-orange">selengkapnya ...</a></p>
                     </div>
                 </div>
             </div>
