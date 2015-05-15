90256e0480a459ba303c2534819df06c6381d6b3
Iwan A Fatahi <iwanfatahi@gmail.com>
2015-05-15 10:23:59 +0700

    Perubahan content sedikit dan gambar

--- Web/function/latest-news.php
+++ Web/function/latest-news.php
@@ -3,14 +3,20 @@
         <div class="top-project-info" style="padding: 10px 10px 15px 10px !important">
             <div class="content-info-short clearfix">
                 <div class="grid_3 clearfix" style="margin-right: 100px !important">
-                    <a class="thumb-img" href="#" style="height:204px !important;width: 292px !important; padding-bottom: 3px;">
-                        <img alt="$TITLE" src="images/projects/small/2.jpg">
+                    <a class="thumb-img" href="#" style="!important;width: 292px !important; padding-bottom: 3px;">
+                        <img alt="$TITLE" width="292px" src="images/crowdfunding.jpg">
                     </a>
                 </div>
                 <div class="grid_7">
                     <div class="wrap-short-detail" style="width: 100%">
-                        <h3 class="rs acticle-title"><a href="#" class="be-fc-orange">Lorem Ipsum</a></h3>
-                        <p class="rs title-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi convallis erat eu fermentum malesuada. Nulla facilisi. Etiam venenatis turpis diam, ut gravida leo porta malesuada. Suspendisse nulla odio, commodo id feugiat et, tincidunt at sem. Nullam urna eros, malesuada a eros nec, tempus consectetur ligula. Ut maximus tortor ac diam venenatis aliquam. Maecenas maximus tellus finibus scelerisque placerat. Pellentesque sit amet velit vehicula, consectetur erat non, condimentum nisl. Nullam maximus hendrerit quam, et vulputate dui molestie ut. Etiam sodales eu risus vitae efficitur. <a href="#" class="be-fc-orange">selengkapnya ...</a></p>
+                        <h3 class="rs acticle-title"><a href="#" class="be-fc-orange">Crowdfunding di Indonesia</a></h3>
+                        <p class="rs title-description">
+                   Crowdfunding bisa menjadi salah satu sektor startup yang menarik untuk dilirik di Indonesia, mengingat negara ini memiliki budaya gotong royong yang kental. Crowdfunding sejatinya merupakan sebuah konsep menggalang pendanaan dari publik atau masyarakat. Dengan konsep ini, penggalang dana bisa terbantu untuk menjalankan proyeknya. Di sisi lain, pemberi dana bisa mendapatkan berbagai keuntungan yang ditawarkan oleh pemilik proyek. <br/><br/>
+
+		    Sejak tahun 2012, startup crowdfunding lokal sudah mulai bermunculan di tanah air. Meskipun demikian sangat sedikit sekali crowdfunding yang melirik skema syariah. Croowd.co.id merupakan salah satu pelopor di bidang ini yang sangat konsen untuk membantu UMKM di Indonesia. 
+
+ <br/><br/>              
+ <a href="#" class="be-fc-orange">selengkapnya ...</a></p>
                     </div>
                 </div>
             </div>
