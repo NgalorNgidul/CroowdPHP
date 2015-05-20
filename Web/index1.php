b6dda36ec630b30eec2367f488eb84a7d103e75c
Isht <ae.isht@gmail.com>
2015-05-21 04:20:15 +0700

    Local change

--- Web/index1.php
+++ Web/index1.php
@@ -1,20 +1,20 @@
 <?php include"function/header-banner.php"; ?>
 
 <!--end: #home-slider -->
-<!--<div class="home-feature-category">
+<div class="home-feature-category">
     <div class="container_12 clearfix">
-        <div class="grid_4 left-lst-category">-->
-          <!--   <div class="wrap-lst-category">
+        <div class="grid_4 left-lst-category">
+             <div class="wrap-lst-category">
                 <h3 class="title-welcome rs">Welcome to Kickstars!</h3>
                 <p class="description rs">Pellentesque laoreet sapien id lacus luctus non fringilla elit lobortis. Fusce augue diam, tempor posuere pharetra sed, feugiat non sapien.</p>
                 <nav class="lst-category">
-                    <?php // include"function/list-category.php"; ?> 
+                    <?php include"function/list-category.php"; ?> 
                     <p class="rs view-all-category">
                         <a href="#category" id="category" class="be-fc-orange">+ View all categories</a>
                     </p>
                 </nav><!--end: .lst-category -->
-         <!--    </div>  
-        </div><!--end: .left-lst-category --> 
+             </div>  
+        </div><!--end: .left-lst-category  -->
 <!--        <div class="grid_8 marked-category">
             <?php // include "function/hot-thread.php"; ?>
         </div>end: .marked-category 
